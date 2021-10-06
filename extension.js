9f7d9039a151c1ce5b6ab4592f93569e31b01d19
fthx <23138504+fthx@users.noreply.github.com>
2021-10-06 09:58:49 +0200

    Handle Ubuntu GS 40+ modifications

--- extension.js
+++ extension.js
@@ -22,10 +22,22 @@ class Extension {
         Main.layoutManager.connect('startup-complete', () => {
             Main.sessionMode.hasOverview = this._realHasOverview
         });
+        // handle Ubuntu's modified GNOME Shell for Dock
+        try {
+            Main.layoutManager.startInOverview = false;
+        } catch(e) {
+            log("No-Overview extension warning: command not found because not running Ubuntu");
+        }   
     }
 
     disable() {
         Main.sessionMode.hasOverview = this._realHasOverview;
+        // handle Ubuntu's modified GNOME Shell for Dock
+        try {
+            Main.layoutManager.startInOverview = true;
+        } catch(e) {
+            log("No Overview extension warning: command not found because not running Ubuntu");
+        }
     }
 }
 

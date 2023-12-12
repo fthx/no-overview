c4d1546c271ff47d7cec9c4bca3b005d6bd715b1
fthx <23138504+fthx@users.noreply.github.com>
2023-12-12 08:21:47 +0100

    Avoid lockups in GS 45 for some users
    So revert to the old mechanism, aka simply hide the overview at start-up.
    Not so pretty but reliable as overview is fully triggered at start-up.

--- extension.js
+++ extension.js
@@ -1,32 +1,26 @@
 /*
   No overview at start-up
   GNOME Shell 45+ extension
-  Contributors: @fthx, @fmuellner
+  Contributors: @fthx
   License: GPL v3
 */
-
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 export default class NoOverviewExtension {
-    constructor() {
-        this._realHasOverview = Main.sessionMode.hasOverview;
-    }
-
     enable() {
         if (!Main.layoutManager._startingUp) {
             return;
         }
 
-        Main.sessionMode.hasOverview = false;
-
-        this._startup_complete = Main.layoutManager.connect('startup-complete', () => {
-            Main.sessionMode.hasOverview = this._realHasOverview;
-        });
+        Main.layoutManager.connectObject(
+            'startup-complete',
+            () => Main.overview.hide(),
+            this
+        );
     }
 
     disable() {
-        Main.sessionMode.hasOverview = this._realHasOverview;
-        Main.layoutManager.disconnect(this._startup_complete);
+        Main.layoutManager.disconnectObject(this);
     }
 }
